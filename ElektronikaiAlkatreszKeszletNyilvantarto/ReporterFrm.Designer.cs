944091df69c470b1fe40bf4596a611a55892483b
József Pető <pe.jocc@gmail.com>
2022-03-14 12:30:43 +0100

    Riport ablak riportviewerben a felesleges menüelemek kikapcsolása

--- ElektronikaiAlkatreszKeszletNyilvantarto/ReporterFrm.Designer.cs
+++ ElektronikaiAlkatreszKeszletNyilvantarto/ReporterFrm.Designer.cs
@@ -43,18 +43,29 @@ namespace EKNyilvantarto
             this.reportViewer1.Location = new System.Drawing.Point(12, 12);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.ShowBackButton = false;
+            this.reportViewer1.ShowContextMenu = false;
+            this.reportViewer1.ShowCredentialPrompts = false;
+            this.reportViewer1.ShowDocumentMapButton = false;
+            this.reportViewer1.ShowFindControls = false;
+            this.reportViewer1.ShowParameterPrompts = false;
+            this.reportViewer1.ShowProgress = false;
+            this.reportViewer1.ShowPromptAreaButton = false;
+            this.reportViewer1.ShowRefreshButton = false;
+            this.reportViewer1.ShowStopButton = false;
+            this.reportViewer1.ShowZoomControl = false;
             this.reportViewer1.Size = new System.Drawing.Size(760, 508);
             this.reportViewer1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button1.Location = new System.Drawing.Point(688, 526);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 24);
             this.button1.TabIndex = 1;
-            this.button1.Text = "OK";
+            this.button1.Text = "Bezárás";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // ReporterFrm
@@ -68,7 +79,7 @@ namespace EKNyilvantarto
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReporterFrm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ReporterFrm";
+            this.Text = "Riport ablak";
             this.Load += new System.EventHandler(this.ReporterFrm_Load);
             this.ResumeLayout(false);
 
